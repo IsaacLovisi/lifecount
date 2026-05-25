032a2a329d49c291662608b11c1b85f08a957084
Isaac Lovisi <isaaclovisi@gmail.com>
2026-05-25 16:51:53 -0300

    setup inicial

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Ambiente de Comando e Controle - Radar Operacional";
+
+  # Define de onde vamos puxar os pacotes (Nixpkgs unstable garante pacotes atualizados)
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      # Define a arquitetura do seu sistema (mude se estiver usando ARM/Mac)
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # Aqui vão as ferramentas de sistema que o ambiente precisa
+        packages = with pkgs; [
+          nodejs_22
+	  git
+	  mitmproxy
+	  android-tools
+          # O npm já vem embutido no nodejs_20, mas podemos declarar utilitários extras aqui se precisar no futuro
+        ];
+
+        shellHook = ''
+          echo "======================================"
+          echo "    Sistema de Contagem de Faltas     "
+          echo "       V01                            "
+          echo "======================================"
+        '';
+      };
+    };
+}
