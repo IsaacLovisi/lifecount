c587e9e028df5e6009a6332b25bb404d7f821750
isaac lovisi <isaaclovisi@gmail.com>
2026-05-29 00:50:17 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 import { timeStamp } from "node:console";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
-const PORT = 3333;
+const port = Number(process.env.PORT) || 8000;
 
+app.listen(port, () => console.log(`listening on port ${port}!`));
 //Middleware global: converte o corpo das requisições para json
 app.use(express.json());
 
 //Rotas para teste
-app.get('/ping', (req: Request, res: Response) => {
-    res.status(200).json({
-        message: 'Servidor Online',
-        timeStamp: new Date().toISOString()
-    });
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("salve");
+  res.status(200).json({
+    message: "Servidor Online",
+    timeStamp: new Date().toISOString(),
+  });
 });
 
-app.listen(PORT, '127.0.0.1', () => {
-    console.log(`[Servidor] Ativo na porta ${PORT}`);
+app.listen(port, "127.0.0.1", () => {
+  console.log(`[Servidor] Ativo na porta ${port}`);
 });
